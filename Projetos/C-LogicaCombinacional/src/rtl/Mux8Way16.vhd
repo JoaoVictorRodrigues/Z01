994ed4ee61e3da4397524ce0b2708b7334010c75
Marcompp <marcomoliternop@gmail.com>
2018-03-14 17:29:56 -0300

    Arrumando aqui

--- Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
@@ -14,3 +14,24 @@ entity Mux8Way16 is
 			sel: in  STD_LOGIC_VECTOR(2 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+
+architecture rtl of Mux8Way16 is
+begin
+	if (sel = 000) then
+      Z <= a;
+  elsif (sel = 001) then
+      Z <= b;
+  elsif (sel = 010) then
+      Z <= c;
+  elsif (sel = 011) then
+      Z <= d;
+  elsif (sel = 100) then
+      Z <= e;
+  elsif (sel = 101) then
+      Z <= f;
+  elsif (sel = 110) then
+      Z <= g;
+  else then
+      Z <= h;
+  end if;
+end Behavioral;
