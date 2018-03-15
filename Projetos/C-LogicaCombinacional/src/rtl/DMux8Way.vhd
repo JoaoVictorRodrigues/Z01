a78717ef8b029756d4a98c8aad75a6963716fc05
JoseFernandoInsper <josefmc@al.edu.br>
2018-03-15 01:41:23 -0300

    adicionando os ultimos que passaram no teste

--- Projetos/C-LogicaCombinacional/src/rtl/DMux8Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/DMux8Way.vhd
@@ -1,24 +1,36 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity DMux8Way is
-	port (
-			a:   in  STD_LOGIC;
-			sel: in  STD_LOGIC_VECTOR(2 downto 0);
-			q0:  out STD_LOGIC;
-			q1:  out STD_LOGIC;
-			q2:  out STD_LOGIC;
-			q3:  out STD_LOGIC;
-			q4:  out STD_LOGIC;
-			q5:  out STD_LOGIC;
-			q6:  out STD_LOGIC;
-			q7:  out STD_LOGIC);
-end entity;
+    Port ( p : in std_logic;
+           q : out std_logic_vector(7 downto 0);
+           r : in std_logic_vector(2 downto 0));
+end DMux8Way;
 
-architecture rtl of DMux8Way is
+architecture demux18 of DMux8Way is
 
 begin
-
-if sel = "000" a <= q0 else sel = "001" a <= q1 else sel = "010" a <= q2 else sel = "011" a <= q3 else sel = "100" a <= q4 else sel = "101" a <= q5 else sel = "110" a <= q6 else a <= q7;
-
-end rtl;
+process(p,r)
+begin
+case r is
+when "000"=>
+q(0)<=p;
+when "001"=>
+q(1)<=p;
+when "010"=>
+q(2)<=p;
+when "011"=>
+q(3)<=p;
+when "100"=>
+q(4)<=p;
+when "101"=>
+q(5)<=p;
+when "110"=>
+q(6)<=p;
+when others=>
+q(7)<=p;
+end case;
+end process;
+end demux18;
--- Projetos/C-LogicaCombinacional/src/rtl/Mux2Way.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux2Way.vhd
@@ -1,10 +1,26 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity Mux2Way is
-	port ( 
-			a:   in  STD_LOGIC;
-			b:   in  STD_LOGIC;
-			sel: in  STD_LOGIC;
-			q:   out STD_LOGIC);
-end entity;
+--  Uncomment the following lines to use the declarations that are
+--  provided for instantiating Xilinx primitive components.
+--library UNISIM;
+--use UNISIM.VComponents.all
+
+entity mux2Way is
+    Port ( x : in std_logic_vector(1 downto 0);
+           s : in std_logic_vector(1 downto 0);
+           y : out std_logic);
+end mux2Way;
+
+architecture mux of mux2Way is
+
+begin
+  process(x,s)
+  begin
+  if(s="0")then
+    y<=x(0);
+  end if ;
+  end process ;
+end mux;
